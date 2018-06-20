a660466a7f69a5ddf4d88943348c5d6d25652059
Matt Farmer <mrfarmer777@gmail.com>
2018-06-20 03:34:39 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,21 @@
+
+class Application
+  def call(env)
+    resp=Rack::Response.new
+    req=Rack::Request.new(env)
+    if req.path.match(/items/)
+      req_item=req.path.split("/items/").last
+      item=@@items.find{|i| i.name==req_item}
+      if item==nil
+        resp.status=400
+        resp.write "Item not found"
+      else
+        resp.write item.price
+      end
+    else
+      resp.status=404
+      resp.write "Route not found"
+    end
+    resp.finish
+  end
+end
