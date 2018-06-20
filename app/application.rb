029a1c7e6a7c517fc80d7f8444d6e4556be8ea4b
Matt Farmer <mrfarmer777@gmail.com>
2018-06-20 03:26:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@
 class Application
   def call(env)
     resp=Rack::Response.new
-    req=Rack":Request.new(env)
+    req=Rack::Request.new(env)
     if req.path.match(/items/)
       req_item=req.path.split("/items/").last
       item=@@items.find{|i| i.name=req_item}
